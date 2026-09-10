b5872acc7e33fa42c85eca47e0c05ee9c862029d
IbnuBrilliant <146622249+qwerty890-boty@users.noreply.github.com>
2026-09-10 09:58:04 +0700

    Add files via upload

---
+++ pekan2/pekan2/String_2611532024.py
@@ -0,0 +1,15 @@
+#Buat file dengan nama String _NIM.py
+#nama variable ditambah 4 digit nim terakhir contoh: nama_1234
+#ganti programnya dan ddata masing2
+nama_2024="Ibnu Brilliant"
+jenis_kelamin_2024='L'
+alamat_2024="""
+ kampus Unand 
+ Kec. Pauh 
+ Kota Padang
+"""
+agama_2024='islam'
+print("Nama saya adalah:", nama_2024)
+print("Jenis Kelamin saya adalah:",jenis_kelamin_2024)
+print("Alamat saya adalah:",alamat_2024)
+print("Agama saya adalah:",agama_2024)
