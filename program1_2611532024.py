b62030dbedaa3cdfa52aface1ed9dc0448c60108
IbnuBrilliant <146622249+qwerty890-boty@users.noreply.github.com>
2026-09-10 14:44:40 +0700

    Restore program1 pekan1

---
+++ program1_2611532024.py
@@ -0,0 +1,12 @@
+#program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 pratikum alpro
+"""
+print("Hello World")
+print()
+print("program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "di buat oleh a\n" + "Ibnu Brilliant_2611532024")
